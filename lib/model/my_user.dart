69569ef94196bf62753b10657f03aad3c681b4c0
osama gabr <osamagabr940@gmail.com>
2025-11-09 15:11:11 +0200

    splash screen

--- lib/model/my_user.dart
+++ lib/model/my_user.dart
@@ -4,24 +4,16 @@ class MyUser {
   String name;
   String email;
 
-  MyUser({
-    required this.id,
-    required this.name,
-    required this.email,
-  });
+  MyUser({required this.id, required this.name, required this.email});
 
   MyUser.fromFireStore(Map<String, dynamic> data)
-      : this(
-          id: (data['id'] ?? '') as String,
-          name: (data['name'] ?? '') as String,
-          email: (data['email'] ?? '') as String,
-        );
+    : this(
+        id: (data['id'] ?? '') as String,
+        name: (data['name'] ?? '') as String,
+        email: (data['email'] ?? '') as String,
+      );
 
   Map<String, dynamic> toFireStore() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-    };
+    return {'id': id, 'name': name, 'email': email};
   }
 }
