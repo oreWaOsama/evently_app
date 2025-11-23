b6e2c27e02453bbb0a31c446a44815456a2e3751
osama gabr <osamagabr940@gmail.com>
2025-11-23 16:11:29 +0200

    feat: implement Google Maps integration with a new map tab and platform API key configurations

--- lib/feature/home/taps/map/map_tab.dart
+++ lib/feature/home/taps/map/map_tab.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MapTab extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapTab extends StatefulWidget {
   const MapTab({super.key});
+
+  @override
+  State<MapTab> createState() => _MapTabState();
+}
+
+class _MapTabState extends State<MapTab> {
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Map Tap')),
-      body: Center(
-        child: Text(
-          'This is the Map Tap',
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {},
       ),
     );
   }
